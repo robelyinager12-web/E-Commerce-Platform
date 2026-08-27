dbb0c8e33570f8acf6877a09d388c2eadc14fcdb
robel <robelyinager12@gmail.com>
2026-08-27 09:03:01 +0300

    Wire up cart route and protected wishlist route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,7 +5,10 @@ import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { ProductListing } from "../pages/ProductListing";
 import { ProductDetail } from "../pages/ProductDetail";
+import { Cart } from "../pages/Cart";
+import { Wishlist } from "../pages/Wishlist";
 import { NotFound } from "../pages/NotFound";
+import { ProtectedRoute } from "../components/common/ProtectedRoute";
 
 export function AppRoutes() {
   return (
@@ -16,8 +19,14 @@ export function AppRoutes() {
         <Route path="/register" element={<Register />} />
         <Route path="/products" element={<ProductListing />} />
         <Route path="/products/:slug" element={<ProductDetail />} />
-        {/* Cart, wishlist, checkout, and account pages are added in the
-            following frontend steps. */}
+        <Route path="/cart" element={<Cart />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/wishlist" element={<Wishlist />} />
+        </Route>
+
+        {/* Checkout and account pages are added in the following
+            frontend steps. */}
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
