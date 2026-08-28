04dab36e39655ccfd6d15610fe9eb093a9fc73c4
robel <robelyinager12@gmail.com>
2026-08-28 09:14:26 +0300

    Wire up account route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -10,6 +10,7 @@ import { Wishlist } from "../pages/Wishlist";
 import { Checkout } from "../pages/Checkout";
 import { OrderHistory } from "../pages/OrderHistory";
 import { OrderDetail } from "../pages/OrderDetail";
+import { Account } from "../pages/Account";
 import { NotFound } from "../pages/NotFound";
 import { ProtectedRoute } from "../components/common/ProtectedRoute";
 
@@ -29,9 +30,9 @@ export function AppRoutes() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/orders" element={<OrderHistory />} />
           <Route path="/orders/:id" element={<OrderDetail />} />
+          <Route path="/account" element={<Account />} />
         </Route>
 
-        {/* Account pages are added in a following frontend step. */}
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
